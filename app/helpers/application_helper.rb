0a2f3413fc6384bff159248bc946a53e665e8317
nguyenhai1478 <nguyen.khac.hai@framgia.com>
2015-01-26 15:49:53 +0700

    staticpages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_t = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_t.empty?
+      base_title
+    else
+      "#{page_t} | #{base_title}"
+    end
+  end
 end
